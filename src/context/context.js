4f3ea21199f97c09cbb5bf1620cd9d916d32de9d
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-03 01:38:36 +0500

    Create context

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GithubContext = createContext();
+
+// Provider , consumer 
+
+
+const GithubProvider = (props) => {
+   return (
+      <GithubContext.Provider value={'hello'}>
+         {props.children}
+      </GithubContext.Provider>
+   )
+}
+
+export {GithubProvider, GithubContext}
