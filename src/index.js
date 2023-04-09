9cfdd23d801e548cc041e2b87e0ac36efa5aca4e
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-04-10 02:43:08 +0500

    Remove the social login error page bug

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ root.render(
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
+      cacheLocation='localstorage'
     >
       <GithubProvider>
         <App />
